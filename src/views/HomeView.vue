f31823083c0b44530039461fab75d4c200006e8d
nagisa2 <lazycoder254@gmail.com>
2022-04-23 16:06:00 +0300

    ui sample

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="home">
-    <Nav />
-    <div class="home-view">
-      <div class="home-a">
+    <Nav ref="nav" />
+    <div class="home-view" ref="homeView">
+      <div class="home-a" ref="homeA">
         <h3>
           Mars digital creation is a <span>design</span> and large format
           printing company based in <span>Nairobi,kenya</span>
         </h3>
       </div>
-      <div class="home-b">
+      <div class="home-b" ref="homeB">
         <span class="home-mid-img">
           <img src="../assets/mars.jpg" alt="middle img" class="mid-img" />
         </span>
       </div>
     </div>
-    <div>
+    <div ref="client">
       <Client />
     </div>
     <div>
-      <div class="service">
+      <div class="service" ref="service">
         <h3>service</h3>
         <h1>check out our service offered</h1>
         <Service />
       </div>
-      <div class="project">
+      <div class="project" ref="project">
         <h3>project</h3>
         <h1>Enjoy the show</h1>
         <ProjectCard />
       </div>
-      <div>
+      <div ref="done">
         <Done />
       </div>
-        <div>
+      <div ref="comment">
         <Comment />
       </div>
-      <div>
+      <div ref="contact">
         <Contact />
       </div>
     </div>
@@ -48,8 +48,8 @@ import Done from "../components/projectsDone.vue";
 import Client from "../components/ClientComponent.vue";
 import ProjectCard from "../components/ProjectCard.vue";
 import Comment from "../components/CommentComponent.vue";
-
 export default {
+  
   components: {
     Nav,
     Done,
